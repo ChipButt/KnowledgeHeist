cc67bc3b5bdef51c84cacac569b768fb32848390
ChipButt <jameschipbutt@hotmail.com>
2026-04-21 11:44:42 +0100

    Update debug.js

--- js/debug.js
+++ js/debug.js
@@ -1,29 +1,12 @@
 export const DEBUG = {
   interaction: false,
-  layoutOverlay: true
+  layoutOverlay: false
 };
 
 export function drawInteractionDebug() {
-  /* intentionally disabled */
+  /* debug disabled */
 }
 
-export function drawLayoutOverlay({
-  ctx,
-  helpers
-}) {
-  if (!DEBUG.layoutOverlay) return;
-
-  const exit = helpers.getExitZone();
-  if (!exit) return;
-
-  ctx.save();
-
-  ctx.fillStyle = 'rgba(70, 70, 70, 0.38)';
-  ctx.fillRect(exit.x1, exit.y1, exit.x2 - exit.x1, exit.y2 - exit.y1);
-
-  ctx.strokeStyle = 'rgba(52, 52, 52, 0.95)';
-  ctx.lineWidth = 2;
-  ctx.strokeRect(exit.x1, exit.y1, exit.x2 - exit.x1, exit.y2 - exit.y1);
-
-  ctx.restore();
+export function drawLayoutOverlay() {
+  /* debug disabled */
 }
